761c278d4648a2e9c651f4874339a1e22046323d
ernestguevarra <ernestgmd@gmail.com>
2018-01-25 02:06:16 +0000

    update function scripts

--- R/ageChildren.R
+++ R/ageChildren.R
@@ -5,26 +5,45 @@
 #' Goodness of fit to an expected (model-based) age distribution
 #'
 #' @param age Vector of ages
-#' @param u4mr Under five years mortality rate as deaths / 10,000 persons / day
+#' @param u5mr Under five years mortality rate as deaths / 10,000 persons / day
+#' @param groups Age groupings specified as recodes parameter in the
+#'     \code{recode} function; default is
+#'     \code{"6:17=1; 18:29=2; 30:41=3; 42:53=4; 54:59=5"}
 #' @return A list of class "ageChildren" with:
-#'     \describe{
-#'       \item{\code{u5mr}}{Under five years mortality rate as deaths / 10000 persons / day}
-#'       \item{\code{observed}}{Table of counts in each (year-centred) age group}
-#'       \item{\code{expected}}{Table of expected counts in each (year-centred) age group}
-#'       \item{\code{X2}}{Chi-squared test statistic}
-#'       \item{\code{df}}{Degrees of freedom for Chi-squared test}
-#'       \item{\code{p}}{p-value for Chi-squared test}
-#'     }
+#' \describe{
+#' \item{\code{u5mr}}{Under five years mortality rate as deaths / 10000 persons / day}
+#' \item{\code{observed}}{Table of counts in each (year-centred) age group}
+#' \item{\code{expected}}{Table of expected counts in each (year-centred) age group}
+#' \item{\code{X2}}{Chi-squared test statistic}
+#' \item{\code{df}}{Degrees of freedom for Chi-squared test}
+#' \item{\code{p}}{p-value for Chi-squared test}
+#' }
 #' @examples
-#'     #
+#' # Chi-Squared test for age of children in dp.ex02 sample dataset using an
+#' # \code{u5mr} of 1 / 10,000 / day.
+#' svy <- dp.ex02
+#' ac <- ageChildren(svy$age, u5mr = 1)
+#' ac
 #'
+#' # Apply function to each sex separately
+#' # Males
+#' acM <- ageChildren(svy$age[svy$sex == 1], u5mr = 1)
+#' acM
+#' # Females
+#' acF <- ageChildren(svy$age[svy$sex == 2], u5mr = 1)
+#'
+#' # Simplified call to function by sex
+#' by(svy$age, svy$sex, ageChildren, u5mr = 1)
+#' @export
 #'
 #
 ################################################################################
 
-ageChildren <- function(age, u5mr = 0, groups = "6:17=1; 18:29=2; 30:41=3; 42:53=4; 54:59=5")
-  {
-  ycag <- recode(age, "6:17=1; 18:29=2; 30:41=3; 42:53=4; 54:59=5")
+ageChildren <- function(age,
+                        u5mr = 0,
+                        groups = "6:17=1; 18:29=2; 30:41=3; 42:53=4; 54:59=5") {
+
+  ycag <- recode(age, groups)
   z <- (u5mr / 10000) * 365.25
   t <- 0:4
   p <- exp(-z * 0:4)
@@ -35,21 +54,65 @@ ageChildren <- function(age, u5mr = 0, groups = "6:17=1; 18:29=2; 30:41=3; 42:53
   observed <- fullTable(ycag, values = 1:5)
   X2 <- sum((observed - expected)^2 / expected)
   pX2 <- pchisq(X2, df = 4, lower.tail = FALSE)
-  result <- list(u5mr = u5mr, observed = observed, expected = expected, X2 = X2, df = 4, p = pX2)
+  result <- list(u5mr = u5mr,
+                 observed = observed,
+                 expected = expected,
+                 X2 = X2,
+                 df = 4,
+                 p = pX2)
   class(result) <- "ageChildren"
   return(result)
-  }
 
-print.ageChildren <- function(x)
-  {
+}
+
+
+################################################################################
+#
+#' print.ageChildren
+#'
+#' \code{print()} helper function for \code{ageChildren()} function
+#'
+#' @param x Object resulting from applying \code{ageChildren()} function
+#' @param ... Additional \code{print()} arguments
+#' @return Printed output of \code{ageChildren()} function
+#' @examples
+#' # Print Chi-Squared test for age of children in dp.ex02 sample dataset using
+#' # an \code{u5mr} of 1 / 10,000 / day.
+#' svy <- dp.ex02
+#' ac <- ageChildren(svy$age, u5mr = 1)
+#' print(ac)
+#' @export
+#
+################################################################################
+
+print.ageChildren <- function(x, ...) {
   cat("\n\tAge Test (Children)\n\n", sep = "")
   cat("X-squared = ", formatC(x$X2, format = "f", width = 6), ", df = ", x$df, ", p = ", formatC(x$p, format = "f", width = 6), "\n\n", sep = "")
-  }
+}
 
-plot.ageChildren <- function(x)
-  {
+
+################################################################################
+#
+#' plot.ageChildren
+#'
+#' \code{plot()} helper function for \code{ageChildren()} function
+#'
+#' @param x Object resulting from applying \code{ageChildren()} function
+#' @param ... Additional \code{barplot()} graphical parameters
+#' @return Bar plot comparing table of observed counts vs table of expected counts
+#' @examples
+#' # Plot Chi-Squared test for age of children in dp.ex02 sample dataset using
+#' # an \code{u5mr} of 1 / 10,000 / day.
+#' svy <- dp.ex02
+#' ac <- ageChildren(svy$age, u5mr = 1)
+#' plot(ac)
+#' @export
+#
+################################################################################
+
+plot.ageChildren <- function(x, ...) {
   YLIM = c(0, max(max(x$observed), max(x$expected)))
   par(mfcol = c(1, 2))
   barplot(x$observed, main = "Observed", ylim = YLIM)
   barplot(x$expected, main = "Expected", ylim = YLIM)
-  }
+}
--- R/nipnTK.R
+++ R/nipnTK.R
@@ -7,9 +7,15 @@
 #'
 #' @docType package
 #' @name nipnTK
-#' @export
-#' @importFrom
+#' @importFrom stats IQR chisq.test cov dnorm mahalanobis na.omit pchisq pnorm
+#'     prop.test qqline qqnorm quantile sd var
+#' @importFrom graphics abline axTicks axis barplot hist lines par plot rect
+#'     strheight strwidth text
+#' @importFrom bbw recode
 NULL
+
+## quiets concerns of R CMD check re: the psus and THRESHOLD that appear in bbw
+#if(getRversion() >= "2.15.1")  utils::globalVariables(c("psu", "THRESHOLD"))
 
 ################################################################################
 #
@@ -21,16 +27,253 @@ NULL
 #'
 #' @format A data frame with 593 observations and 10 variables
 #' \describe{
-#' \item{\code{psu}{Primary sampling unit}
-#' \item{\code{camp}{Camp name code}
-#' \item{\code{block}{Block code}
-#' \item{\code{age}{Age (years)}
-#' \item{\code{sex}{Sex}
-#' \item{\code{weight}{Weight (kg)}
-#' \item{\code{height}{Height (cm)}
-#' \item{\code{demispan}{Demispan (cm)}
-#' \item{\code{muac}{Mid-upper arm circumference (cm)}
-#' \item{\code{oedema}{Oedema}
+#' \item{\code{psu}}{Primary sampling unit}
+#' \item{\code{camp}}{Camp name code}
+#' \item{\code{block}}{Block code}
+#' \item{\code{age}}{Age (years)}
+#' \item{\code{sex}}{Sex}
+#' \item{\code{weight}}{Weight (kg)}
+#' \item{\code{height}}{Height (cm)}
+#' \item{\code{demispan}}{Demispan (cm)}
+#' \item{\code{muac}}{Mid-upper arm circumference (cm)}
+#' \item{\code{oedema}}{Oedema}
 #' }
-#' @source Data courtesy of \link{HelpAge International}{http://www.helpage.org}
+#' @source Data courtesy of \href{HelpAge International}{http://www.helpage.org}
 "ah.ex01"
+
+
+################################################################################
+#
+#' Example dataset for age and sex distributions function
+#'
+#' Data taken from household rosters collected as part of a household survey in
+#' Tanzania.
+#'
+#' @format A data frame of 8736 observations and 2 variables
+#' \describe{
+#' \item{\code{age}}{Age (years)}
+#' \item{\code{sex}}{Sex (1 = Male / 2 = Female)}
+#' }
+"as.ex01"
+
+
+################################################################################
+#
+#' Example dataset for age and sex distributions function
+#'
+#' Census data of Tanzania taken from the Wolfram|Alpha knoweldge engine.
+#'
+#' @format A data frame with 20 observations and 4 variables
+#' \describe{
+#' \item{\code{age}}{Age group}
+#' \item{\code{Males}}{Total male population}
+#' \item{\code{Females}}{Total female population}
+#' \item{\code{All}}{Total population}
+#' }
+#' @source \url{http://www.wolframalpha.com/input/?i=Tanzania+age+distribution}
+"as.ex02"
+
+
+################################################################################
+#
+#' Example dataset for distributions of variables and indices
+#'
+#' Anthropometric data from a SMART survey in Kabul, Afghanistan.
+#'
+#' @format A data frame with 873 observations and 11 variables
+#' \describe{
+#' \item{\code{psu}}{Primary sampling unit}
+#' \item{\code{age}}{Age of child (months)}
+#' \item{\code{sex}}{Gender of child}
+#' \item{\code{weight}}{Weight of child (kgs)}
+#' \item{\code{height}}{Height of child (cm)}
+#' \item{\code{muac}}{Mid-upper arm circumference (mm)}
+#' \item{\code{oedema}}{Presence or absence of oedema}
+#' \item{\code{haz}}{Height-for-age z-score}
+#' \item{\code{waz}}{Weight-for-age z-score}
+#' \item{\code{whz}}{Weight-for-height z-score}
+#' \item{\code{flag}}{Data quality flag}
+#' }
+"dist.ex01"
+
+
+################################################################################
+#
+#' Example dataset for digit preference function
+#'
+#' Anthropometric data from a single state from a Demographic and Health Survey
+#' (DHS) of a West African country.
+#'
+#' @format A data frame with 796 observations and 6 variables
+#' \describe{
+#' \item{\code{psu}}{Primary sampling unit}
+#' \item{\code{age}}{Age (months)}
+#' \item{\code{sex}}{Gender}
+#' \item{\code{wt}}{Weight (kg)}
+#' \item{\code{ht}}{height (cm)}
+#' \item{\code{oedema}}{Presence or absence of oedema}
+#' }
+"dp.ex01"
+
+
+################################################################################
+#
+#' Example dataset for digit preference function
+#'
+#' Anthropometric data from a SMART survey in Kabul, Afghanistan in a
+#' comma-separated-value (CSV) file format. This is a survey of children aged
+#' 6-59 months old.
+#'
+#' @format A data frame with 873 observations and 7 variables
+#' \describe{
+#' \item{\code{psu}}{Primary sampling unit}
+#' \item{\code{age}}{Age of child (months)}
+#' \item{\code{sex}}{Gender of child}
+#' \item{\code{weight}}{Weight of child (kgs)}
+#' \item{\code{height}}{Height of child (cm)}
+#' \item{\code{muac}}{Mid-upper arm circumference (mm)}
+#' \item{\code{oedema}}{Presence or absence of oedema}
+#' }
+"dp.ex02"
+
+
+################################################################################
+#
+#' Example dataset for digit preference
+#'
+#' Anthropometric data for a sample of children living in a refugee camp in a
+#' West African country.
+#'
+#' @format A data frame with 374 observations and 6 variables
+#' \describe{
+#' \item{\code{age}}{Age (months)}
+#' \item{\code{sex}}{Gender (1 = Male / 2 = Female)}
+#' \item{\code{weight}}{Weight (kg)}
+#' \item{\code{height}}{Height (cm)}
+#' \item{\code{muac}}{Mid-upper arm circumference (mm)}
+#' \item{\code{oedema}}{Presence or absence of oedema}
+#' }
+"dp.ex03"
+
+
+################################################################################
+#
+#' Example dataset for identifying outliers using flags
+#'
+#' Anthropometric data from a SMART survey in Sudan.
+#'
+#' @format A data frame with 786 observations and 11 variables
+#' \describe{
+#' \item{\code{psu}}{Primary sampling unit}
+#' \item{\code{child}}{Child ID}
+#' \item{\code{age}}{Age (months)}
+#' \item{\code{sex}}{Gender (1 = Male / 2 = Female)}
+#' \item{\code{weight}}{Weight (kg)}
+#' \item{\code{height}}{Height (cm)}
+#' \item{\code{muac}}{Mid-upper arm circumference (mm)}
+#' \item{\code{oedema}}{Presence or absence of oedema}
+#' \item{\code{haz}}{Height-for-age z-score}
+#' \item{\code{waz}}{Weight-for-age z-score}
+#' \item{\code{whz}}{Weight-for-height z-score}
+#' }
+"flag.ex01"
+
+
+################################################################################
+#
+#' Example dataset for identifying outliers using flags
+#'
+#' Anthropometric data from a survey of children 11 years or older attending
+#' school in Ethiopia.
+#'
+#' @format A data.frame with 973 observations and 7 variables.
+#' \describe{
+#' \item{\code{school}}{School ID}
+#' \item{\code{sex}}{Gender (1 = Male / 2 = Female)}
+#' \item{\code{ageMonths}}{Age (months)}
+#' \item{\code{weight}}{Weight (kg)}
+#' \item{\code{height}}{Height (cm)}
+#' \item{\code{haz}}{Height-for-age z-score}
+#' \item{\code{baz}}{Body mass index (BMI)-for-age z-score}
+#' }
+"flag.ex02"
+
+
+################################################################################
+#
+#' Example dataset for identifying outliers using flags
+#'
+#' Anthropometric data from a national survey in Nigeria.
+#'
+#' @format A data frame with 18330 observations and 10 variables
+#' \describe{
+#' \item{\code{psu}}{Primary sampling unit}
+#' \item{\code{region}}{Region code}
+#' \item{\code{state}}{State}
+#' \item{\code{age}}{Age (months)}
+#' \item{\code{sex}}{Gender (1 = Male / 2 = Female)}
+#' \item{\code{weight}}{Weight (kg)}
+#' \item{\code{height}}{Height (cm)}
+#' \item{\code{haz}}{Height-for-age z-score}
+#' \item{\code{waz}}{Weight-for-age z-score}
+#' \item{\code{whz}}{Weight-for-height z-score}
+#' }
+"flag.ex03"
+
+
+################################################################################
+#
+#' Example dataset for checking ranges and legal values
+#'
+#' Anthropometric data from a SMART survey in Angola.
+#'
+#' @format A data frame with 906 observations and 6 variables
+#' \describe{
+#' \item{\code{age}}{Age (months)}
+#' \item{\code{sex}}{Gender (1 = Male / 2 = Female)}
+#' \item{\code{weight}}{Weight (kg)}
+#' \item{\code{height}}{Height (cm)}
+#' \item{\code{muac}}{Mid-upper arm circumference (mm)}
+#' \item{\code{oedema}}{Presence or absence of oedema}
+#' }
+"rl.ex01"
+
+
+################################################################################
+#
+#' Example dataset for using scatterplots to identify outliers
+#'
+#' Anthropometric data from a SMART survey in the Democratic Republic of Congo.
+#'
+#' @format A data frame with 895 observations and 6 variables
+#' \describe{
+#' \item{\code{age}}{Age (months)}
+#' \item{\code{sex}}{Gender (1 = Male / 2 = Female)}
+#' \item{\code{weight}}{Weight (kg)}
+#' \item{\code{height}}{Height (cm)}
+#' \item{\code{muac}}{Mid-upper arm circumference (mm)}
+#' \item{\code{oedema}}{Presence or absence of oedema}
+#' }
+"sp.ex01"
+
+
+################################################################################
+#
+#' Example dataset for using scatterplots to identify outliers
+#'
+#' Anthropometric data from a survey of school-age (i.e., between 5 and 15 years)
+#' children from Pakistan
+#'
+#' @format A data frame with 849 observations and 9 variables
+#' \describe{
+#' \item{\code{region}}{Region code}
+#' \item{\code{school}}{School code}
+#' \item{\code{ageMonths}}{Age (months)}
+#' \item{\code{sex}}{Sex (1 = Male / 2 = Female)}
+#' \item{\code{weight}}{Weight (kg)}
+#' \item{\code{height}}{Height (cm)}
+#' \item{\code{haz}}{Height-for-age z-score}
+#' \item{\code{waz}}{Weight-for-age z-score}
+#' \item{\code{baz}}{Body mass index (BMI)-for-age z-score}
+#' }
+"sp.ex02"
