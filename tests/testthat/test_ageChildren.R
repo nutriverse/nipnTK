e1715cdb2de05c4e4c1dc4d27745557f4fc8889f
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2018-01-25 21:03:27 +0000

    add tests for ageChildren

---
+++ tests/testthat/test_ageChildren.R
@@ -0,0 +1,22 @@
+library(nipnTK)
+context("Test ageChildren")
+
+svy <- dp.ex02
+ac <- ageChildren(svy$age, u5mr = 1)
+
+test_that("ac is class ageChildren", {
+  expect_is(ac, "ageChildren")
+})
+
+test_that("ac is list", {
+  expect_true(is.list(ac))
+})
+
+test_that("names of elements of ac", {
+  expect_match(names(ac[1]), "u5mr")
+  expect_match(names(ac[2]), "observed")
+  expect_match(names(ac[3]), "expected")
+  expect_match(names(ac[4]), "X2")
+  expect_match(names(ac[5]), "df")
+  expect_match(names(ac[6]), "p")
+})
