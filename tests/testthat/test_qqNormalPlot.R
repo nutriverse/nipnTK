3e0f00ba9765db38570fca608b771fb4351ca0df
ernestguevarra <ernestgmd@gmail.com>
2018-01-26 21:34:20 +0000

    add test for qqNormalPlot

---
+++ tests/testthat/test_qqNormalPlot.R
@@ -0,0 +1,12 @@
+library(nipnTK)
+context("Tests for qqNormalPlot")
+
+svy <- dist.ex01
+qqNormalPlot(svy$muac)
+qqNormalPlot(svy$haz)
+qqNormalPlot(svy$waz)
+qqNormalPlot(svy$whz)
+
+test_that("qqNormalPlot is NULL", {
+  expect_is(qqNormalPlot, "NULL")
+})
