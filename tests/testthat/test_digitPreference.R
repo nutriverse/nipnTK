963977d99e9aedfa18e6336aef3f5148c9d0929c
ernestguevarra <ernestgmd@gmail.com>
2018-01-26 05:56:35 +0000

    add tests for digitPreference

---
+++ tests/testthat/test_digitPreference.R
@@ -0,0 +1,20 @@
+library(nipnTK)
+context("Tests for digitPreference")
+
+svy <- dp.ex01
+dp <- digitPreference(svy$wt, digits = 1)
+
+test_that("dp is digitPreference", {
+  expect_is(dp, "digitPreference")
+})
+
+test_that("dp is list", {
+  expect_true(is.list(dp))
+})
+
+test_that("names of dp", {
+  expect_equal(names(dp)[1], "dps")
+  expect_equal(names(dp)[2], "tab")
+  expect_equal(names(dp)[3], "pct")
+  expect_equal(names(dp)[4], "dpsClass")
+})
