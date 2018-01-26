75e6629547d2995907c03abee95cd8c294cd9e6d
ernestguevarra <ernestgmd@gmail.com>
2018-01-26 05:45:55 +0000

    update ageRatioTest tests

--- tests/testthat/test_ageRatioTest.R
+++ tests/testthat/test_ageRatioTest.R
@@ -7,3 +7,17 @@ art <- ageRatioTest(svy$age, ratio = 0.85)
 test_that("art is ageRatioTest", {
   expect_is(art, "ageRatioTest")
 })
+
+test_that("art is list", {
+  expect_true(is.list(art))
+})
+
+test_that("names of art", {
+  expect_equal(names(art)[1], "expectedR")
+  expect_equal(names(art)[2], "expectedP")
+  expect_equal(names(art)[3], "observedR")
+  expect_equal(names(art)[4], "observedP")
+  expect_equal(names(art)[5], "X2")
+  expect_equal(names(art)[6], "df")
+  expect_equal(names(art)[7], "p")
+})
