a5d3e58bcf132b2b3c8f445aa832344da886cf11
Ernest Guevarra <ernest@guevarra.io>
2020-07-29 02:52:25 +0100

    update recode

--- R/recode.R
+++ R/recode.R
@@ -98,10 +98,14 @@ recode <- function(var, recodes, afr, anr = TRUE, levels) {
     result <- if (!missing(levels)) factor(result, levels = levels) else as.factor(result)
   } else if (anr && (!is.numeric(result))) {
       result.valid <- na.omit(result)
-      opt <- options(warn = -1)
-      result.valid <- as.numeric(result.valid)
-      options(opt)
-      if (!any(is.na(result.valid))) result <- as.numeric(result)
+      withr::with_options(
+        new = options(warn = -1),
+        code = {
+          result.valid <- as.numeric(result.valid)
+      #options(opt)
+          if (!any(is.na(result.valid))) result <- as.numeric(result)
+        }
+      )
   }
   return(result)
 }
