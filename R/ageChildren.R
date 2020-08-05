d11bf8e6857fd0b5112e3741e6defa2f4f1f7457
Ernest Guevarra <ernest@guevarra.io>
2020-08-05 22:26:18 +0100

    update ageChildren and ageHeaping functions

--- R/ageChildren.R
+++ R/ageChildren.R
@@ -5,20 +5,22 @@
 #' @param age Vector of ages
 #' @param u5mr Under five years mortality rate as deaths / 10,000 persons / day
 #' @param groups Age groupings specified as recodes parameter in the
-#'     \code{recode} function; default is
+#'     \code{\link[bbw]{recode}} function; default is
 #'     \code{"6:17=1; 18:29=2; 30:41=3; 42:53=4; 54:59=5"}
+#'
 #' @return A list of class "ageChildren" with:
 #' \describe{
-#' \item{\code{u5mr}}{Under five years mortality rate as deaths / 10000 persons / day}
-#' \item{\code{observed}}{Table of counts in each (year-centred) age group}
-#' \item{\code{expected}}{Table of expected counts in each (year-centred) age group}
-#' \item{\code{X2}}{Chi-squared test statistic}
-#' \item{\code{df}}{Degrees of freedom for Chi-squared test}
-#' \item{\code{p}}{p-value for Chi-squared test}
+#'   \item{\code{u5mr}}{Under five years mortality rate as deaths / 10000 persons / day}
+#'   \item{\code{observed}}{Table of counts in each (year-centred) age group}
+#'   \item{\code{expected}}{Table of expected counts in each (year-centred) age group}
+#'   \item{\code{X2}}{Chi-squared test statistic}
+#'   \item{\code{df}}{Degrees of freedom for Chi-squared test}
+#'   \item{\code{p}}{p-value for Chi-squared test}
 #' }
+#'
 #' @examples
 #' # Chi-Squared test for age of children in dp.ex02 sample dataset using an
-#' # \code{u5mr} of 1 / 10,000 / day.
+#' # u5mr of 1 / 10,000 / day.
 #' svy <- dp.ex02
 #' ac <- ageChildren(svy$age, u5mr = 1)
 #' ac
@@ -32,6 +34,7 @@
 #'
 #' # Simplified call to function by sex
 #' by(svy$age, svy$sex, ageChildren, u5mr = 1)
+#'
 #' @export
 #'
 #
@@ -40,8 +43,7 @@
 ageChildren <- function(age,
                         u5mr = 0,
                         groups = "6:17=1; 18:29=2; 30:41=3; 42:53=4; 54:59=5") {
-
-  ycag <- recode(age, groups)
+  ycag <- bbw::recode(age, groups)
   z <- (u5mr / 10000) * 365.25
   t <- 0:4
   p <- exp(-z * 0:4)
@@ -51,7 +53,7 @@ ageChildren <- function(age,
   names(expected) <- 1:5
   observed <- fullTable(ycag, values = 1:5)
   X2 <- sum((observed - expected)^2 / expected)
-  pX2 <- pchisq(X2, df = 4, lower.tail = FALSE)
+  pX2 <- stats::pchisq(X2, df = 4, lower.tail = FALSE)
   result <- list(u5mr = u5mr,
                  observed = observed,
                  expected = expected,
@@ -66,18 +68,22 @@ ageChildren <- function(age,
 
 ################################################################################
 #
-#' \code{print()} helper function for \code{ageChildren()} function
+#' \code{print} helper function for \code{ageChildren} function
 #'
-#' @param x Object resulting from applying \code{ageChildren()} function
-#' @param ... Additional \code{print()} arguments
-#' @return Printed output of \code{ageChildren()} function
+#' @param x Object resulting from applying \code{\link{ageChildren}} function
+#' @param ... Additional \code{\link{print}} arguments
+#'
+#' @return Printed output of \code{\link{ageChildren}} function
+#'
 #' @examples
 #' # Print Chi-Squared test for age of children in dp.ex02 sample dataset using
-#' # an \code{u5mr} of 1 / 10,000 / day.
+#' # an u5mr of 1 / 10,000 / day.
 #' svy <- dp.ex02
 #' ac <- ageChildren(svy$age, u5mr = 1)
 #' print(ac)
+#'
 #' @export
+#'
 #
 ################################################################################
 
@@ -89,24 +95,28 @@ print.ageChildren <- function(x, ...) {
 
 ################################################################################
 #
-#' \code{plot()} helper function for \code{ageChildren()} function
+#' \code{plot} helper function for \code{ageChildren} function
 #'
-#' @param x Object resulting from applying \code{ageChildren()} function
-#' @param ... Additional \code{barplot()} graphical parameters
+#' @param x Object resulting from applying \code{\link{ageChildren}} function
+#' @param ... Additional \code{\link{barplot}} graphical parameters
+#'
 #' @return Bar plot comparing table of observed counts vs table of expected counts
+#'
 #' @examples
 #' # Plot Chi-Squared test for age of children in dp.ex02 sample dataset using
-#' # an \code{u5mr} of 1 / 10,000 / day.
+#' # an u5mr of 1 / 10,000 / day.
 #' svy <- dp.ex02
 #' ac <- ageChildren(svy$age, u5mr = 1)
 #' plot(ac)
+#'
 #' @export
+#'
 #
 ################################################################################
 
 plot.ageChildren <- function(x, ...) {
   YLIM = c(0, max(max(x$observed), max(x$expected)))
   par(mfcol = c(1, 2))
-  barplot(x$observed, main = "Observed", ylim = YLIM)
-  barplot(x$expected, main = "Expected", ylim = YLIM)
+  graphics::barplot(x$observed, main = "Observed", ylim = YLIM)
+  graphics::barplot(x$expected, main = "Expected", ylim = YLIM)
 }
